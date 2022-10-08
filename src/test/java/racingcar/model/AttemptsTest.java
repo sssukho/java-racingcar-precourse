1148b5e73f940924de37cb0ea56e745895de0d61
sssukho <sssukho@gmail.com>
2022-10-08 20:04:50 +0900

    reactor: Attempts(클래스#10) 리팩토링
    - 변수명 수정
    - 메소드명 수정

--- src/test/java/racingcar/model/AttemptsTest.java
+++ src/test/java/racingcar/model/AttemptsTest.java
@@ -8,19 +8,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class AttemptsTest {
-
-    @DisplayName("시도 횟수 예외 테스트")
+    @DisplayName("시도 횟수 입력 예외 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"asdf", "-1", "-123", "0", "1a", "12fv", "fv12"})
-    void fail(String attempts) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new Attempts(attempts));
+    @ValueSource(strings = {"asdf", "-1", "-123", "0", "1a", "12fv", "fv12", " "})
+    void fail(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Attempts(input));
     }
 
-    @DisplayName("시도 횟수 성공 테스트")
+    @DisplayName("시도 횟수 입력 성공 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"1", "2", "3", "4", "01"})
-    void successString(String attempts) {
-        assertThat(new Attempts(attempts).getAttempts()).isEqualTo(Integer.parseInt(attempts));
+    @ValueSource(strings = {"1", "2", "3", "4", "01", "101", "333"})
+    void success(String input) {
+        assertThat(new Attempts(input).getCount()).isEqualTo(Integer.parseInt(input));
     }
 }
